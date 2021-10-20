3de674c9d0253c1ca63964520ea517d0d7cbd729
cyrus937 <cyrillekenfack4@gmail.com>
2021-10-20 09:18:13 +0100

    ajout du changement de thème

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:talks/view/state_management/theme_provider.dart';
+import 'package:talks/view/widgets/change_theme_button_widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,25 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: Mythemes.lightTheme,
+            themeMode: themeProvider.themeMode,
+            darkTheme: Mythemes.darkTheme,
+            home: MyHomePage(title: 'Flutter Demo Home Page'),
+          );
+        },
+      );
 }
 
 class MyHomePage extends StatefulWidget {
@@ -73,6 +71,9 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: [
+          ChangeThemeButtonWidget(),
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
